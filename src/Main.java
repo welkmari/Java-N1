1276877532a420db10e7280dfd3164b5bfcece39
MARIA HELENA WELK <maria_welk@estudante.sesisenai.org.br>
2025-10-21 20:28:43 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //cria o leitor do terminal
+        Scanner entrada = new Scanner(System.in);
+
+        int g;
+        System.out.print("Digite um valor para 'g':");
+        //le o valor digitado e armazena em "g"
+        g = entrada.nextInt();
+
+        System.out.println("O valor de 'g' é:" + g);
+
     }
 }
+
